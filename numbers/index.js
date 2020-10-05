ea6f825d9789ba7ee1382039361a6de8a7d77a32
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-05 16:03:04 -0500

    added all of the packages and folders. Now preparing to dive in

---
+++ numbers/index.js
@@ -0,0 +1,17 @@
+const isEven = (num) => {
+  // write code for numbers.isEven
+};
+
+const sum = (arr) => {
+  // write code for numbers.sum
+};
+
+const comboSum = (arr, sum) => {
+  // write code for numbers.comboSum
+};
+
+module.exports = {
+  isEven,
+  sum,
+  comboSum,
+};
