988f71ae7a2484bb6efea8aefdd80abd915352a6
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-08 12:35:53 -0500

    did all of the functions

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,19 +1,40 @@
 const isEven = (num) => {
   // write code for numbers.isEven
   // Return a boolean that represents whether a given number is even
-  // Hint: Modulus
+  // Hint: the modulus function returns the division remainer
+
+  // If the number is divisible by 2 then return true else return false
+  // In order to be even a number must be dvisible by 2
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const sum = (arr) => {
   // write code for numbers.sum
   // Return a value that represents the sum of an array of numbers
-  // Hint: Reduce
+  // Hint: Reduce method reduces the array to a single value
+
+  // syntax: array.reduce(function([total],[CurrentValue],[currentIndex],[arr],[initialValue])
+  return arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
 };
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
   // Determine if any combination of numbers in an array equals a given sum. Return a boolean
   // Hint: Loop in loop OR indexOf for diff
+  let startingArray = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  if (startingArray == sum) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,13 +1,21 @@
 const split = (str, delim) => {
   // write code for strings.split
+  let newString = str.split(delim);
+  return newString;
 };
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let arr = str.match(/.{1,2}/g);
+  return arr;
 };
 
 const reverse = (str) => {
   // write code for strings.reverse
+  let wordSplitter = str.split("");
+  let wordReverse = wordSplitter.reverse("");
+  let newWord = wordReverse.join("");
+  return newWord;
 };
 
 module.exports = {
